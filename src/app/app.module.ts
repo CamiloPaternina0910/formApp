8a20876b153ba927a982c5f4f033d36d85613df9
Camilo Paternina <cpaterninapertuz95@correo.unicordoba.edu.co>
2023-04-25 20:11:54 -0500

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { ReactiveModule } from './reactive/reactive.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -14,8 +12,6 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule,
-    ReactiveModule,
     SharedModule
   ],
   providers: [],
