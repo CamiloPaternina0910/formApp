ef222966ad38686e999fd7c96181115d6716573a
Camilo Paternina <cpaterninapertuz95@correo.unicordoba.edu.co>
2023-04-26 10:34:56 -0500

    errors

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  myForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)], []),
+    price: new FormControl(0, [Validators.required, Validators.min(0)], []),
+    inStorage: new FormControl(0, [Validators.required, Validators.min(0)], [])
+  });
+
+  onSave(){
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value)
+    this.myForm.reset()
+  }
+
+
+  hasErrors(field: string) : boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null{
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for( const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        
+        case 'minlength':
+          return `Este campo debe tener mínimo ${errors['minlength'].requiredLength}`
+      }
+    }
+
+    return null;
+  }
 }
